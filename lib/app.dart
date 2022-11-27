086e857d4054fd546a1746402183b355a2e800cf
zilurrane <zilurrane@gmail.com>
2022-11-27 19:59:37 +0530

    Added AppStateNotifier

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,20 @@
-import 'package:chat/shared/theme/theme_data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'shared/theme/theme_data.dart';
+import 'shared/utils/app_state_notifier.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Chat',
-        theme: ThemeConfig.lightTheme,
-        darkTheme: ThemeConfig.darkTheme,
-        home: const Text('Chat'),
-        debugShowCheckedModeBanner: false);
+    return Consumer<AppStateNotifier>(builder: (context, appState, child) {
+      return MaterialApp(
+          title: 'Chat',
+          theme: ThemeConfig.lightTheme,
+          darkTheme: ThemeConfig.darkTheme,
+          home: const Text('Chat'),
+          debugShowCheckedModeBanner: false);
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
-import 'package:chat/app.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'app.dart';
+import 'shared/utils/app_state_notifier.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider<AppStateNotifier>(
+      create: (_) => AppStateNotifier(),
+      child: const App(),
+    ),
+  );
 }
