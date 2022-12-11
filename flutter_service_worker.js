57bcb4612c32e156ad8eebc7943817ca59fc562f
zilurrane <zilurrane@gmail.com>
2022-12-11 20:58:25 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4084578ea1b67041dac151c1aa64121b",
-"/": "4084578ea1b67041dac151c1aa64121b",
-"main.dart.js": "d54230797654a2e68837f76a815d2529",
+"index.html": "f3c5029013f495a11f721038fa502375",
+"/": "f3c5029013f495a11f721038fa502375",
+"main.dart.js": "41e20c60ade3d9da85cb365c70071ae6",
 "manifest.json": "8fb212bd05708f02648ab2e3bebce9e7",
 "version.json": "8e7bd9742e1f54b1ca5c443ab0e74c96"
 };
